a16552d7099e539ca86f010622d8d448abbf9a00
ysmath0246 <heeju060689@gmail.com>
2025-06-09 01:28:30 +0900

    ✨ Add deploy action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/attendance/', // ← 반드시 저장소 이름과 일치
+  base: '/attendance/',    // 저장소 이름과 일치
   plugins: [react()],
+  build: {
+    outDir: 'docs',        // ← dist 대신 docs 로
+    emptyOutDir: true,     // 기존 docs 폴더를 빌드 전에 비워 줌
+  },
 });
