dca1ed3ef67287a9f5c7d609872e514aee68cadf
ysmath0246 <heeju060689@gmail.com>
2025-06-09 12:05:50 +0900

    🐛 전체 변경사항 반영

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   base: '/attendance/',    // 저장소 이름과 일치
   plugins: [react()],
   build: {
+
     outDir: 'docs',        // ← dist 대신 docs 로
     emptyOutDir: true,     // 기존 docs 폴더를 빌드 전에 비워 줌
   },
